455531ef315fbe861a26ce0adb23fe6a01883583
Ángel <angel.olivares04@inacapmail.cl>
2025-09-25 15:09:37 -0300

    a

--- ControlVentas/inventario/urls.py
+++ ControlVentas/inventario/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_productos, name='lista_productos'),
-    path('productos/agregar/', views.agregar_producto, name='agregar_producto'),
-    path('productos/editar/<int:pk>/', views.editar_producto, name='editar_producto'),
-    path('productos/eliminar/<int:pk>/', views.eliminar_producto, name='eliminar_producto'),
-    
-    path('ventas/', views.lista_ventas, name='lista_ventas'),
-    path('ventas/registrar/', views.registrar_venta, name='registrar_venta'),
-    path('clientes/registrar/<str:rut>/', views.registrar_cliente_con_rut, name='registrar_cliente_con_rut'),
+    path("", views.lista_productos, name="lista_productos"),
+    path("productos/agregar/", views.agregar_producto, name="agregar_producto"),
+    path("productos/editar/<int:pk>/", views.editar_producto, name="editar_producto"),
+    path("productos/eliminar/<int:pk>/", views.eliminar_producto, name="eliminar_producto"),  
+    path("ventas/", views.lista_ventas, name="lista_ventas"),
+    path("ventas/registrar/", views.registrar_venta, name="registrar_venta"),
+    path("clientes/registrar/<str:rut>/", views.registrar_cliente_con_rut, name="registrar_cliente_con_rut"),
 ]
--- ControlVentas/inventario/views.py
+++ ControlVentas/inventario/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.db import transaction
+# from django.db import transaction
 from .models import Producto, Cliente, Venta
 from .forms import ProductoForm, ClienteForm, VentaForm
 
