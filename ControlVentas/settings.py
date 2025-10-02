30bcaa2ba112cbb64cfff81272c3687844fdd363
Ángel <angel.olivares04@inacapmail.cl>
2025-10-02 15:10:51 -0300

    static settings

--- ControlVentas/settings.py
+++ ControlVentas/settings.py
@@ -16,6 +16,11 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = BASE_DIR / "public"
+STATIC_URL = '/'
+STATICFIELD_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -117,8 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
