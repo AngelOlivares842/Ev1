529e8c84a1963bc766bf68665f7343e7cf47b765
Ángel <angel.olivares04@inacapmail.cl>
2025-10-02 19:50:24 -0300

    Enable dynamic database configuration via env var
    Switches database settings to use DATABASE_URL from environment if available, falling back to SQLite otherwise. This improves flexibility for deployment across different environments.

--- ControlVentas/settings.py
+++ ControlVentas/settings.py
@@ -79,24 +79,17 @@ WSGI_APPLICATION = 'ControlVentas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
