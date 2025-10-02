bc0c57fc255241c35e2ef6e8381a38c8bf45ebf5
Ángel <angel.olivares04@inacapmail.cl>
2025-10-02 19:49:11 -0300

    Set SQLite as default database in settings
    Commented out environment-based database selection and hardcoded SQLite as the default database engine. This simplifies configuration by always using SQLite regardless of environment variables.

--- ControlVentas/settings.py
+++ ControlVentas/settings.py
@@ -79,17 +79,24 @@ WSGI_APPLICATION = 'ControlVentas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
