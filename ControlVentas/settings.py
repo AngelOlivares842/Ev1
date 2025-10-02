2c9331241e94865dde63cf3be6d1715b8d1709dc
Ángel <angel.olivares04@inacapmail.cl>
2025-10-02 19:13:10 -0300

    Fix static files config and update database settings
    Corrects the STATICFILES_DIRS variable name in settings.py and updates database configuration to use PostgreSQL if DATABASE_URL is set, otherwise defaults to SQLite. Also cleans up requirements.txt formatting.

--- ControlVentas/settings.py
+++ ControlVentas/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFIELD_DIRS = [
+STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
@@ -81,11 +81,17 @@ WSGI_APPLICATION = 'ControlVentas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
-    )
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
