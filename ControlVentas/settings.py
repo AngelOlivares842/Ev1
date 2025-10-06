959a95c08d62e5b9a064fb500a354a5938473f0b
Ángel <angel.olivares04@inacapmail.cl>
2025-10-06 14:42:09 -0300

    fix: database

--- ControlVentas/settings.py
+++ ControlVentas/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,13 +77,7 @@ WSGI_APPLICATION = 'ControlVentas.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
--- inventario/admin.py
+++ inventario/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import  Cliente,Producto,Venta
 # Register your models here.
-admin.site.register(Cliente, Producto, Venta)
+admin.site.register(Cliente)
+admin.site.register(Producto)
+admin.site.register(Venta)
