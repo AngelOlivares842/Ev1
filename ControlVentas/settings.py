f7684e4ebf0c65fcfa17084042f6f0e7a280449e
Ángel <angel.olivares04@inacapmail.cl>
2025-10-23 15:04:28 -0300

    Switch database to PostgreSQL and add dotenv support
    Updated settings.py to use PostgreSQL with environment variables loaded via python-dotenv. Removed the SQLite database file and added python-dotenv to requirements.txt for environment variable management.

--- ControlVentas/settings.py
+++ ControlVentas/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,12 +81,15 @@ WSGI_APPLICATION = 'ControlVentas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DBNAME"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": os.getenv("HOST"),
+        "PORT":os.getenv("PORT")  
     }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
