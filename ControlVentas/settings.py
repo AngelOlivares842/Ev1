8cdb729359704c40fba2e84784da7ada4ad3a59e
Ángel <angel.olivares04@inacapmail.cl>
2025-11-16 22:27:30 -0300

    Add new CORS origin for deployment environment

--- ControlVentas/settings.py
+++ ControlVentas/settings.py
@@ -93,6 +93,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4321",  # Puerto de Astro en desarrollo
     "http://127.0.0.1:4321",
     "http://localhost:3000",  # Por si usas otro puerto
+    'https://tienda-astro-django-n0v5esyl2-angels-projects-72ef0e8e.vercel.app',
 ]
 
 CORS_ALLOW_METHODS = [
